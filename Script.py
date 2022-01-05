c3a152cd08fcf6b4a470353cf2dcda8abbb2ba43
UMESH NAMO <65161113+umeshnamo@users.noreply.github.com>
2022-01-05 21:13:48 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -34,7 +34,7 @@ class script(object):
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
+2. supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
@@ -65,7 +65,7 @@ class script(object):
     EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
 
 <b>NOTE:</b>
-these are the extra features of Eva Maria
+these are the extra features
 
 <b>Commands and Usage:</b>
 • /id - <code>get id of a specifed user.</code>
