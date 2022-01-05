03f711cab88d6c710d3c052064dd9ae71dabf83b
UMESH NAMO <65161113+umeshnamo@users.noreply.github.com>
2022-01-05 13:41:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -39,8 +39,7 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/DHK_Movies')
+            InlineKeyboardButton('🎞️ Group', url='https://t.me/DHK_Movies')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
@@ -80,8 +79,7 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/DHK_Movies')
+            InlineKeyboardButton('🎞️ Group', url='https://t.me/DHK_Movies')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
